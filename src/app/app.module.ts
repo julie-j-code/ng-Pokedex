747a346fcc375d835dc8bc0f9ddf724d027195f5
Julie J <jeannet.julie@gmail.com>
2022-04-25 17:59:57 +0200

    Refacto avec création module spécifique PokemonModule et routes spécifiques + PageNotFound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { BorderCardDirective } from './border-card.directive';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListPokemonComponent,
-    DetailPokemonComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
     PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    PokemonModule,
     AppRoutingModule
   ],
   providers: [],
