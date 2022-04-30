25d5edc7b35f1bdf7abcbb5a0250080230ebf219
Julie J <jeannet.julie@gmail.com>
2022-04-30 14:06:31 +0200

    SearchPokemon component

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -11,6 +11,7 @@ import { PokemonService } from './pokemon.service';
 import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
 import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 import { AddPokemonComponent } from './add-pokemon/add-pokemon.component';
+import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 
 const routesPokemon: Routes = [
 
@@ -28,7 +29,8 @@ const routesPokemon: Routes = [
     PokemonTypeColorPipe,
     PokemonFormComponent,
     EditPokemonComponent,
-    AddPokemonComponent
+    AddPokemonComponent,
+    SearchPokemonComponent
   ],
   imports: [
     CommonModule,
